8e1dba61737ecaec5bcd39804aec0c5f90efdffd
Phoenix1460 <jbmcustomer@icloud.com>
2026-04-21 17:40:21 -0400

    Delete catapults.js

--- catapults.js
+++
@@ -1,8 +0,0 @@
-export function updateCatapult(plane, catapultZones) {
-    catapultZones.forEach(zone => {
-        if (zone.active && zone.ready) {
-            // inject forward velocity
-            plane.vel[0] += zone.force;
-        }
-    });
-}
